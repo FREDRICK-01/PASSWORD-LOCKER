70a02a8ae27fa01c3f36e89a13012d821b22ff11
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-09-07 00:21:58 +0300

    adds a gennerate password function

--- password.py
+++ password.py
@@ -80,4 +80,19 @@ class Credentials():
         for credential in cls.credentials_list:
             if credential.account == account:
                 return True
-        return False          
+        return False     
+
+    @classmethod    
+    def display_credentials(cls):
+        """
+        Method that returns all items in the credentials list.
+        """     
+        return cls.credentials_list
+
+    def generatePassword(stringLength=10)
+    """
+    Generate a random password string of letters and digits and special characters.
+    """    
+    password = string.ascii_uppercase + string,string.ascii_lowercase + string.digits + "~!@#$%^&*"
+    return ''.join(random.choice(password) for i in range(stringLength))
+
