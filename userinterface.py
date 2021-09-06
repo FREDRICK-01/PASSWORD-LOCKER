323d75ac50452d17a75bc4f8bd0bcfffb305c8cd
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-09-07 01:00:54 +0300

    adds adds some fuctions to delete, find,check generate and copy password

--- userinterface.py
+++ userinterface.py
@@ -28,4 +28,51 @@ def login_user(username,password):
     """
   
     check_user = Credentials.verify_user(username,password)
-    return check_user      
+    return 
+
+def create_new_credential(account,userName,password):
+    """
+    Function that creates new credentials for a given user account
+    """
+    new_credential = Credentials(account,userName,password)
+    return new_credential
+def save_credentials(credentials):
+    """
+    Function to save Credentials to the credentials list
+    """
+    credentials. save_details()
+def display_accounts_details():
+    """
+    Function that returns all the saved credential.
+    """
+    return Credentials.display_credentials()
+
+def delete_credential(credentials):
+    """
+    Function to delete a Credentials from credentials list.
+    """
+    credentials.delete_credentials()
+
+def find_credential(account):
+    """
+    Function that finds a Credentials by an account name and returns the credentials that belong to that account
+    """
+    return Credentials.find_credential(account)
+def check_credendtials(account):
+    """
+    Function that check if  credentials exists with that account name and return true or false.
+    """
+    return Credentials.if_credential_exist(account)    
+
+def generate_Password():
+    """
+    Generates a random password for the user.
+    """
+    auto_password=Credentials.generatePassword()
+    return auto_password
+def copy_password(account):
+    """
+    A function that copies the password using the pyperclip framework
+    We import the framework then declare a function that copies the emails.
+    """
+    return Credentials.copy_password(account)
